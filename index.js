7e945f2e32abb460b476ef5992b6101bc9c1c6a2
Oladepo Israel <oladepoisrael913@gmail.com>
2026-09-18 15:37:51 +0100

    last commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const productRoutes = require("./routes/productRoutes.js");
 const port = 8000;
 const mongoose = require("mongoose");
 const compass_string = "mongodb://localhost:27017/oladepoisrael913_db";
-const atlas_string =  process.env.ATLAS_STRING || "mongodb+srv://oladepoisrael913:olade
+const atlas_string = process.env.ATLAS_STRING || "mongodb+srv://oladepoisrael913:Yanuzard%40123@cluster0.u9mlh5i.mongodb.net/oladepoisrael913_db?appName=Cluster0";
 mongoose
   .connect(atlas_string)
   .then(() => {
@@ -21,8 +21,7 @@ mongoose
 
 // const compass_string = "mongodb://localhost:27017"
 // const atlas_string =
-("mongodb+srv://oladepoisrael913_db_user:Yanuzard@456@cluster0.u9mlh5i.mongodb.net/oladepoisrael913_db?appName=Cluster0o");
-
+mongoose.connect(process.env.MONGO_URI);
 app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Server Is Active");
